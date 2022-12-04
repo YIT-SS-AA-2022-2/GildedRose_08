06844dfe469e4474eb83fb903164afc2f7ae9941
jongddak <hfsmsj316@gmail.com>
2022-12-04 23:50:47 +0900

    GildedRoseApprovalTest작성, TexttestFixture.java => Automated test 로 변경(ApprovalTest 이용)

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -4,18 +4,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GildedRoseTest {
-    // 퀄리티 테스트용 메서드
     private static void QualityTest(Item[] items, GildedRose app) {
         System.out.println("name, sellIn, quality");
-        int days = 7;
-        for (int i = 1; i < days; i++) {
-
+        int days = 6;
+        for (int i = 0; i < days; i++) {
             System.out.println("-------- day " + i + " --------");
             for (Item item : items) {
                 System.out.println(item);
             }
             System.out.println();
             app.updateQuality();
+
         }
     }
     @Test
@@ -30,14 +29,18 @@ public class GildedRoseTest {
     @Test
     public void ItemUpdateQualityTest(){
         Item[] items = new Item[] {
-                new Item("NormalItem", 5, 5),
-                new Item("Aged Brie", 5, 5),
+
+                new Item("+5 Dexterity Vest", 10, 20),
+                new Item("Aged Brie", 2, 0),
                 new Item("Elixir of the Mongoose", 5, 7),
                 new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30),
-                                    };
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("Conjured Mana Cake", 3, 6)
+        };
+
         GildedRose app = new GildedRose(items);
         QualityTest(items, app);
     }
--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,12 +1,12 @@
 package com.gildedrose;
 
 
+import org.junit.jupiter.api.Test;
+
 // 임의로 아이템,days 지정해서 GlidedRose가 잘 굴러가는지 확인
 public class TexttestFixture {
     public static void main(String[] args) {
-        System.out.println("OMGHAI!");
-
-        Item[] items = new Item[] {
+         Item[] items = new Item[] {
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
@@ -15,7 +15,6 @@ public class TexttestFixture {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 6) };
 
         GildedRose app = new GildedRose(items);
@@ -33,7 +32,10 @@ public class TexttestFixture {
             }
             System.out.println();
             app.updateQuality();
+
         }
+
+
     }
 
 }
